9ec408f0367db73666548307fc1c2a2962d71dea
PRIEYAN MN <88473323+PRIEYAN@users.noreply.github.com>
2023-04-10 20:28:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function dark(){
+    document.getElementById('bod').style.background= "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(38,38,38,1) 0%, rgba(18,18,18,1) 77%, rgba(0,0,0,1) 100%)";
+    document.getElementById('footer').style.background='rgb(33, 33, 36)';
+    document.getElementById('cpy').style.background='rgb(22, 22, 26)';
+    document.getElementById('ftitle1').style.color='white';
+    document.getElementById('ftitle2').style.color='white';
+    document.getElementById('ftitle3').style.color='white';
+    document.getElementById('XII').style.background='#00000056';
+    document.getElementById('XI').style.background='#00000056';
+    document.getElementById('X').style.background='#00000056';
+}
+function light(){
+    document.getElementById('bod').style.background='linear-gradient(90deg, rgba(221,221,221,1) 0%, rgba(221,221,221,1) 50%, rgba(221,221,221,1) 100%)';
+    document.getElementById('footer').style.background='gray';
+    document.getElementById('cpy').style.background='rgb(63, 63, 63)';
+    document.getElementById('ftitle1').style.color='black';
+    document.getElementById('ftitle2').style.color='black';
+    document.getElementById('ftitle3').style.color='black';
+    document.getElementById('XII').style.background='#ffffff56';
+    document.getElementById('XI').style.background='#ffffff56';
+    document.getElementById('X').style.background='#ffffff56';
+}
